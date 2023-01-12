d2f4f465f0d454081e1396c1c29e57592724f26f
Lisa Ugray <lisa.ugray@shopify.com>
2023-01-12 10:23:16 -0500

    Rework layout

--- Atreus.ino
+++ Atreus.ino
@@ -25,76 +25,65 @@
 #include "Kaleidoscope-Macros.h"
 #include "Kaleidoscope-TopsyTurvy.h"
 
-
-
-#define MO(n) ShiftToLayer(n)
+#define STL(n) ShiftToLayer(n)
 #define TG(n) LockLayer(n)
 
-enum {
-  LAYER_QWERTY
-};
+#define Sleep LCTRL(LGUI(Key_Q))
+#define WinUp LCTRL(LALT(Key_UpArrow))
+#define WinDown LCTRL(LALT(Key_DownArrow))
+#define WinLeft LCTRL(LALT(Key_LeftArrow))
+#define WinRight LCTRL(LALT(Key_RightArrow))
+#define WinMax LCTRL(LALT(Key_Enter))
+#define Mute Consumer_Mute
+#define VolDown Consumer_VolumeDecrement
+#define VolUp Consumer_VolumeIncrement
+#define LAlt Key_LeftAlt
+#define LCtrl Key_LeftControl
+#define RCtrl Key_RightControl
+#define LCmd Key_LeftGui
+#define RCmd Key_RightGui
+#define LShift Key_LeftShift
+#define RShift Key_RightShift
+#define BkSpc Key_Backspace
+#define SpcBar Key_Spacebar
+#define Up Key_UpArrow
+#define Down Key_DownArrow
+#define Left Key_LeftArrow
+#define Right Key_RightArrow
+#define Key_Colon TOPSY(Semicolon)
+#define Underscore TOPSY(Minus)
+#define Pipe TOPSY(Backslash)
+#define CSA(key) LCTRL(LSHIFT(LALT(Key_ ## key)))
+#define ZZ(key) LGUI(LCTRL(LSHIFT(LALT(Key_ ## key))))
+#define BckSlash Key_Backslash
+#define BckTick Key_Backtick
+#define Del Key_Delete
+#define LParen Key_LeftParen
+#define RParen Key_RightParen
+#define LBrace TOPSY(LeftBracket)
+#define RBrace TOPSY(RightBracket)
+#define LBracket Key_LeftBracket
+#define RBracket Key_RightBracket
 
-#define Key_Exclamation LSHIFT(Key_1)
-#define Key_At LSHIFT(Key_2)
-#define Key_Hash LSHIFT(Key_3)
-#define Key_Dollar LSHIFT(Key_4)
-#define Key_Percent LSHIFT(Key_5)
-#define Key_Caret LSHIFT(Key_6)
-#define Key_And LSHIFT(Key_7)
-#define Key_Star LSHIFT(Key_8)
-#define Key_Plus LSHIFT(Key_Equals)
 
 enum {
-  QWERTY,
-  FUN,
-  UPPER
+  L_QW,
+  L_FN
 };
 
 KEYMAPS(
-  [QWERTY] = KEYMAP_STACKED
-  (
-
-    Key_Q,   Key_W,   Key_E,       Key_R,         Key_T,         /**/
-    Key_A,   Key_S,   Key_D,       Key_F,         Key_G,         /**/
-    Key_Z,   Key_X,   Key_C,       Key_V,         Key_B,         Key_Backtick,
-    Key_Esc, Key_Tab, Key_LeftGui, Key_LeftShift, Key_Backspace, Key_LeftControl,
-
-    /**/           Key_Y,     Key_U,   Key_I,     Key_O,      Key_P,
-    /**/           Key_H,     Key_J,   Key_K,     Key_L,      TOPSY(Semicolon),
-    Key_Backslash, Key_N,     Key_M,   Key_Comma, Key_Period, Key_Slash,
-    Key_LeftAlt,   Key_Space, MO(FUN), Key_Minus, Key_Quote,  Key_Enter
-
+  [L_QW]=KEYMAP(
+    Key_Q,     Key_W, Key_E,    Key_R,     Key_T, /**/     /**/       Key_Y,    Key_U,    Key_I,     Key_O,      Key_P,
+    Key_A,     Key_S, Key_D,    Key_F,     Key_G, /**/     /**/       Key_H,    Key_J,    Key_K,     Key_L,      Key_Colon,
+    Key_Z,     Key_X, Key_C,    Key_V,     Key_B, Key_Tab, Key_Enter, Key_N,    Key_M,    Key_Comma, Key_Period, Key_Slash,
+    STL(L_FN), LAlt,  BckTick,  Key_Minus, BkSpc, LShift,  RShift,    SpcBar,   LCmd,     LCtrl,     Key_Quote,  BckSlash
   ),
-
-  [FUN] = KEYMAP_STACKED
-  (
-
-    Key_Exclamation, Key_At,           Key_UpArrow,   Key_Dollar,           Key_Percent,           /**/
-    Key_LeftParen,   Key_LeftArrow,    Key_DownArrow, Key_RightArrow,       Key_RightParen,        /**/
-    Key_LeftBracket, Key_RightBracket, Key_Hash,      Key_LeftCurlyBracket, Key_RightCurlyBracket, Key_Caret,
-    TG(UPPER),       Key_Insert,       Key_LeftGui,   Key_LeftShift,        Key_Backspace,         Key_LeftControl,
-
-    /**/         Key_PageUp,   Key_7, Key_8,      Key_9, Key_Backspace,
-    /**/         Key_PageDown, Key_4, Key_5,      Key_6, ___,
-    Key_And,     Key_Star,     Key_1, Key_2,      Key_3, Key_Plus,
-    Key_LeftAlt, Key_Space,    ___,   Key_Period, Key_0, Key_Equals
-
-   ),
-
-  [UPPER] = KEYMAP_STACKED
-  (
-
-    Key_Insert, Key_Home,                 Key_UpArrow,   Key_End,        Key_PageUp,   /**/
-    Key_Delete, Key_LeftArrow,            Key_DownArrow, Key_RightArrow, Key_PageDown, /**/
-    XXX,        Consumer_VolumeIncrement, XXX,           XXX,            ___,          ___,
-    M(QWERTY),  Consumer_VolumeDecrement, ___,           ___,            ___,          ___,
-
-    /**/ Key_UpArrow,   Key_F7,    Key_F8,          Key_F9,         Key_F10,
-    /**/ Key_DownArrow, Key_F4,    Key_F5,          Key_F6,         Key_F11,
-    ___, XXX,           Key_F1,    Key_F2,          Key_F3,         Key_F12,
-    ___, ___,           M(QWERTY), Key_PrintScreen, Key_ScrollLock, Consumer_PlaySlashPause
-
-   )
+  [L_FN]=KEYMAP(
+    Key_1,     Key_2, Key_3,    Key_4,     Key_5, /**/     /**/       Key_6,    Key_7,    Key_8,     Key_9,      Key_0,
+    WinLeft,   ___,   WinRight, ___,       ___,   /**/     /**/       Left,     Down,     Up,        Right,      LBrace,
+    ___,       ___,   ___,      ___,       ___,   ___,     WinMax,    LBracket, RBracket, LParen,    RParen,     RBrace,
+    ___,       ___,   ___,      ___,       Del,   Key_Esc, ___,       ___,      ___,      ___,       ___,        Key_Enter
+  )
 )
 /* *INDENT-ON* */
 
@@ -105,9 +94,6 @@ KALEIDOSCOPE_INIT_PLUGINS(
 
 const macro_t *macroAction(uint8_t macroIndex, uint8_t keyState) {
   switch (macroIndex) {
-  case QWERTY:
-    Layer.move(LAYER_QWERTY);
-    break;
   default:
     break;
   }
