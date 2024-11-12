115f7695ffba55a394ee42c7ea4d088f97ffb28c
Lisa Ugray <lisa.ugray@gmail.com>
2024-11-12 09:13:27 -0500

    Swap minus and equals

--- Atreus.ino
+++ Atreus.ino
@@ -87,28 +87,28 @@ enum { M_ESC, M_1, M_2, M_F, M_G, M_UP }; // Macros
 
 KEYMAPS(
   [L_QW]=KEYMAP(
-    Key_Q,      Key_W,     Key_E,         Key_R,     Key_T,  /**/        /**/          Key_Y,  Key_U, Key_I,     Key_O,      Key_P,
-    Key_A,      Key_S,     Key_D,         Key_F,     Key_G,  /**/        /**/          Key_H,  Key_J, Key_K,     Key_L,      Key_Colon,
-    Key_Z,      Key_X,     Key_C,         Key_V,     Key_B,  STL(L_NUM), Underscore,   Key_N,  Key_M, Key_Comma, Key_Period, Key_Quote,
-    Key_Esc,    Key_Minus, LCtrl,         LCmd,      BkSpc,  LShift,     RShift,       SpcBar, RCmd,  RAlt,      Key_Slash,  Key_Equals
+    Key_Q,      Key_W,      Key_E,         Key_R,     Key_T,  /**/        /**/          Key_Y,  Key_U, Key_I,     Key_O,      Key_P,
+    Key_A,      Key_S,      Key_D,         Key_F,     Key_G,  /**/        /**/          Key_H,  Key_J, Key_K,     Key_L,      Key_Colon,
+    Key_Z,      Key_X,      Key_C,         Key_V,     Key_B,  STL(L_NUM), Underscore,   Key_N,  Key_M, Key_Comma, Key_Period, Key_Quote,
+    Key_Esc,    Key_Equals, LCtrl,         LCmd,      BkSpc,  LShift,     RShift,       SpcBar, RCmd,  RAlt,      Key_Slash,  Key_Minus
   ),
   [L_FN]=KEYMAP(
-    Key_1,      Key_2,     Key_3,         Key_4,     Key_5,  /**/        /**/          Key_6,  Key_7, Key_8,     Key_9,      Key_0,
-    BckTick,    ___,       ___,           ___,       ___,    /**/        /**/          Left,   Down,  Up,        Right,      BckTick,
-    ___,        ___,       ___,           ___,       ___,    SpaceLeft,  SpaceRight,   ___,    Mute,  VolDown,   VolUp,      BckSlash,
-    Sleep,      ___,       ___,           ___,       Del,    ___,        ___,          ___,    ___,   ___,       ___,        Sleep
+    Key_1,      Key_2,      Key_3,         Key_4,     Key_5,  /**/        /**/          Key_6,  Key_7, Key_8,     Key_9,      Key_0,
+    BckTick,    ___,        ___,           ___,       ___,    /**/        /**/          Left,   Down,  Up,        Right,      BckTick,
+    ___,        ___,        ___,           ___,       ___,    SpaceLeft,  SpaceRight,   ___,    Mute,  VolDown,   VolUp,      BckSlash,
+    Sleep,      ___,        ___,           ___,       Del,    ___,        ___,          ___,    ___,   ___,       ___,        Sleep
   ),
   [L_M]=KEYMAP(
-    M(M_ESC),   M(M_1),    M(M_2),        ___,       ___,    /**/        /**/          ___,    ___,   ___,       ___,        ___,
-    ___,        ___,       ___,           M(M_F),    M(M_G), /**/        /**/          ___,    ___,   ___,       ___,        ___,
-    ___,        ___,       ___,           ___,       ___,    ___,        ___,          ___,    ___,   ___,       ___,        ___,
-    Sleep,      ___,       ___,           ___,       ___,    ___,        ___,          KMod,   ___,   ___,       ___,        ___
+    M(M_ESC),   M(M_1),     M(M_2),        ___,       ___,    /**/        /**/          ___,    ___,   ___,       ___,        ___,
+    ___,        ___,        ___,           M(M_F),    M(M_G), /**/        /**/          ___,    ___,   ___,       ___,        ___,
+    ___,        ___,        ___,           ___,       ___,    ___,        ___,          ___,    ___,   ___,       ___,        ___,
+    Sleep,      ___,        ___,           ___,       ___,    ___,        ___,          KMod,   ___,   ___,       ___,        ___
   ),
   [L_NUM]=KEYMAP(
-    AM_SMALLER, AM_NEXT,   AM_PREV,       AM_BIGGER, ___,    /**/        /**/          ___,    Key_7, Key_8,     Key_9,      ___,
-    AM_TALL,    AM_WIDE,   AM_FULLSCREEN, ___,       ___,    /**/        /**/          ___,    Key_4, Key_5,     Key_6,      ___,
-    ___,        ___,       ___,           ___,       ___,    ___,        AM_MAKE_MAIN, ___,    Key_1, Key_2,     Key_3,      Key_Enter,
-    ___,        ___,       ___,           ___,       ___,    ___,        ___,          ___,    Key_0, ___,       Key_Period, ___
+    AM_SMALLER, AM_NEXT,    AM_PREV,       AM_BIGGER, ___,    /**/        /**/          ___,    Key_7, Key_8,     Key_9,      ___,
+    AM_TALL,    AM_WIDE,    AM_FULLSCREEN, ___,       ___,    /**/        /**/          ___,    Key_4, Key_5,     Key_6,      ___,
+    ___,        ___,        ___,           ___,       ___,    ___,        AM_MAKE_MAIN, ___,    Key_1, Key_2,     Key_3,      Key_Enter,
+    ___,        ___,        ___,           ___,       ___,    ___,        ___,          ___,    Key_0, ___,       Key_Period, ___
   ),
 )
 /* *INDENT-ON* */
